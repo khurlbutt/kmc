7c954ab3f09cab05eb8a509434b3e224ab0dd895
Kevin Crain <khurlbutt@gmail.com>
2017-09-10 12:49:08 -0700

    [process] Remove boolean attribute for storing if performed or not. No need for it. Assume not performed.

--- data/process.py
+++ data/process.py
@@ -20,7 +20,6 @@ class Process(object):
         self._is_enabled_still_fn = is_enabled_still_fn
         # TODO: Only 2-tuple, e.g. index of function template, cell indices
         self._perform_fn = perform_fn
-        self._is_performed = False
 
     def time_value(self):
         return int(self._est_perform_time)
@@ -29,21 +28,16 @@ class Process(object):
         if not self.is_still_performable():
             raise LatticeProcessException("Not performable")
         self._perform_fn(self.cell)
-        self._is_performed = True
 
     def is_still_performable(self):
         # Assumes that references to cell sites updated elsewhere. Eventually,
         # may need to reference neighbors using cell.row or cell.col.
-        return not self._is_performed and self._is_enabled_still_fn(self.cell)
-
-    def is_performed(self):
-        return bool(self._is_performed)
+        return self._is_enabled_still_fn(self.cell)
 
     def __repr__(self):
         return "%r" % [
             self._est_perform_time,
             (self.cell.row, self.cell.col),
-            ("is_performed", self.is_performed()),
             ("_is_enabled_still_fn", self._is_enabled_still_fn),
             ("_perform_fn", self._perform_fn)]
 
