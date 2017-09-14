c4537b4af2457034f6c646d0208cbadc974f355c
Kevin Crain <khurlbutt@gmail.com>
2017-09-13 23:38:49 -0700

    nit: move simulation.run() to top of definition, provides valuable context for reading the rest. generally make flake8 pass after Kevin tYpEd SoMeThInG

--- data/simulation.py
+++ data/simulation.py
@@ -14,14 +14,21 @@ class Simulation(object):
         self.STOP_TIME = -1.0
         self.STOP_STEP = 100
 
-        # 
         # Enable processes
         # Draw a process, perform if possible, add new enabled processes.
         # (Repeat above)
 
+    def run(self):
+        while self._continue_sim():
+            next_process = self.process_queue.pop()
+            if next_process.is_still_performable(self.lattice):
+                self.step += 1
+                self.time = next_process.occurence_time
+                next_process.perform(self.lattice)
+                self._update_process_queue(next_process.sites)
+
     def _initialize_lattice(self):
         self.lattice = data.lattice.Lattice()
-        return lattice
 
     def _initialize_process_queue(self):
         self.process_queue = data.enabled_collection.EnabledCollection(
@@ -41,22 +48,12 @@ class Simulation(object):
         # TODO...
         return set()
 
-
     def _continue_sim(self):
         # TODO include more flexibility on stop conditions
         if self.STOP_TIME > 0:
             return self.STOP_TIME > self.time
         else:
             return self.STOP_STEP > self.step
-
-    def run(self):
-        while self._continue_sim():
-            next_process = self.process_queue.pop()
-            if next_process.is_still_performable(self.lattice):
-                self.step += 1
-                self.time = next_process.occurence_time
-                next_process.perform(self.lattice)
-                self._update_process_queue(next_process.sites)
 
     def __repr__(self):
         return "t=%d\nLattice:%r\nEnabledCollection:%r" % (
