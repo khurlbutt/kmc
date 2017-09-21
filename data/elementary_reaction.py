cdec330ba2105142c597a7f50d0c34abe09b2f51
Kevin Hurlbutt <khurlbutt@gmail.com>
2017-09-20 22:38:18 -0700

    Now has the elementary step class. Big things coming...

---
+++ data/elementary_reaction.py
@@ -0,0 +1,55 @@
+"""
+File: elementary_reaction.py
+----------------------------
+This class defines an elementary step for a chemical reaction catalyzed on a 
+metal surface.
+"""
+
+class ElementaryStep():
+	"""docstring for ElementaryStep"""
+	def __init__(self, name="elem rxn", k=1.0, E=None, definition=None):
+		self.name = name
+		self.k = k
+		self.E = E
+		self.definition = definition
+		
+	def justify_definition(self):
+		min_x = float("inf")
+		min_y = float("inf")
+		for key in self.definition:
+			x = key[0]
+			if x<min_x:
+				min_x = x
+			y = key[1]
+			if y<min_y:
+				min_y = y
+		justified_def = {}
+		for key in spec:
+			x = key[0]
+			y = key[1]
+			justified_def[(x-min_x, y-min_y, s)] = spec[key]
+		self.definition = justified_def
+
+	def create_rotated_elem_rxns(self):
+		rot_90 = {}
+		rot_180 = {}
+		rot_270 = {}
+		for key in spec:
+			x = key[0]
+			y = key[1]
+			rot_90[(y, -x)] = spec[key]
+			rot_180[(-y, -x)] = spec[key]
+			rot_270[(-y, x)] = spec[key]
+		result = [
+					ElementaryStep(self.name, self.k, self.E, rot_90),
+					ElementaryStep(self.name, self.k, self.E, rot_180),
+					ElementaryStep(self.name, self.k, self.E, rot_270)
+					]
+		return result
+
+	def arrhenius(self, dE, prefactor=1e13):
+		pass
+
+
+
+
