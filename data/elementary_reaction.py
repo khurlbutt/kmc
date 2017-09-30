a0d5cd839bf9427b44de63db7a6e9d70084d7efc
Kevin Crain <khurlbutt@gmail.com>
2017-09-30 02:27:11 -0700

    [elem_rxn] Mostly just a flake8 and 1 or 2 minor nits.
    @khurlbutt - into what you're onto here but am a little confused until we can discuss what the rest of transplanting https://github.com/khurlbutt/kmc/blob/master/settings/elem_rxns_configs/v3/toy_A.py#L38-L74 and/or https://github.com/khurlbutt/kmc/blob/master/settings/elem_rxns_configs/v4/toy_A.py#L37-L84 should look like (the direction makes sense, but we need to discuss moar befoar I do accidentally do something silly). Doesn't define 'spec' so the code won't run and fails flake8 for now

--- data/elementary_reaction.py
+++ data/elementary_reaction.py
@@ -1,55 +1,50 @@
 """
 File: elementary_reaction.py
 ----------------------------
-This class defines an elementary step for a chemical reaction catalyzed on a 
+This class defines an elementary step for a chemical reaction catalyzed on a
 metal surface.
 """
 
 class ElementaryStep():
-	"""docstring for ElementaryStep"""
-	def __init__(self, name="elem rxn", k=1.0, E=None, definition=None):
-		self.name = name
-		self.k = k
-		self.E = E
-		self.definition = definition
-		
-	def justify_definition(self):
-		min_x = float("inf")
-		min_y = float("inf")
-		for key in self.definition:
-			x = key[0]
-			if x<min_x:
-				min_x = x
-			y = key[1]
-			if y<min_y:
-				min_y = y
-		justified_def = {}
-		for key in spec:
-			x = key[0]
-			y = key[1]
-			justified_def[(x-min_x, y-min_y, s)] = spec[key]
-		self.definition = justified_def
+    """docstring for ElementaryStep"""
+    def __init__(self, name=None, k=1.0, E=None, definition=None):
+        self.name = name or "elem rxn"
+        self.k = k
+        self.E = E
+        self.definition = definition
 
-	def create_rotated_elem_rxns(self):
-		rot_90 = {}
-		rot_180 = {}
-		rot_270 = {}
-		for key in spec:
-			x = key[0]
-			y = key[1]
-			rot_90[(y, -x)] = spec[key]
-			rot_180[(-y, -x)] = spec[key]
-			rot_270[(-y, x)] = spec[key]
-		result = [
-					ElementaryStep(self.name, self.k, self.E, rot_90),
-					ElementaryStep(self.name, self.k, self.E, rot_180),
-					ElementaryStep(self.name, self.k, self.E, rot_270)
-					]
-		return result
+    def justify_definition(self):
+        min_x = float("inf")
+        min_y = float("inf")
+        for key in self.definition:
+            x = key[0]
+            if x < min_x:
+                min_x = x
+            y = key[1]
+            if y < min_y:
+                min_y = y
+        justified_def = {}
+        for key in spec:
+            x = key[0]
+            y = key[1]
+            justified_def[(x - min_x, y - min_y, s)] = spec[key]
+        self.definition = justified_def
 
-	def arrhenius(self, dE, prefactor=1e13):
-		pass
+    def create_rotated_elem_rxns(self):
+        rot_90 = {}
+        rot_180 = {}
+        rot_270 = {}
+        for key in spec:
+            x = key[0]
+            y = key[1]
+            rot_90[(y, -x)] = spec[key]
+            rot_180[(-y, -x)] = spec[key]
+            rot_270[(-y, x)] = spec[key]
+        return [
+            ElementaryStep(self.name, self.k, self.E, rot_90),
+            ElementaryStep(self.name, self.k, self.E, rot_180),
+            ElementaryStep(self.name, self.k, self.E, rot_270)
+        ]
 
-
-
-
+    def arrhenius(self, dE, prefactor=1e13):
+        pass
